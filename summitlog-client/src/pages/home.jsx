fe2caa4ff4fac096bf402d6d980214527b2c3a33
Isaiah Scheel <isaiahscheel@gmail.com>
2019-09-13 12:48:27 -0700

    Homepage Cards

--- summitlog-client/src/pages/home.jsx
+++ summitlog-client/src/pages/home.jsx
@@ -1,19 +1,41 @@
-import React, { Component } from 'react';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
+
+import Hike from '../components/Hike'
 
 class home extends Component {
-    render() {
-        return (
-            <Grid container>
-                <Grid item sm={8} xs={12}>
-                    <p>Content...</p>
-                </Grid>
-                <Grid item sm={4} xs={12}>
-                    <p>Profile...</p>
-                </Grid>
-            </Grid>
-        );
-    }
+  state = {
+    hikes: null
+  };
+  componentDidMount() {
+    axios
+      .get("/hikes")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          hikes: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+  render() {
+    let recentHikes = this.state.hikes ? (
+      this.state.hikes.map(hike => <Hike hike={hike} />)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={6}>
+        <Grid item sm={8} xs={12}>
+          {recentHikes}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-export default home
+export default home;
