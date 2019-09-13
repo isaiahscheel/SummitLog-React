fe85872e18c0895dbff04b02b6474bd75793b3f1
Isaiah Scheel <isaiahscheel@gmail.com>
2019-09-12 22:02:51 -0700

    NavBar for the React App

--- summitlog-client/src/App.js
+++ summitlog-client/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+/**
+ * Import the pages needed
+ */
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
 function App() {
   return (
     <div className="App">
-     <h1>Summit Log - Hello World</h1>
+      <Router>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ summitlog-client/src/components/NavBar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Link from "react-router-dom/Link";
+
+//Material Design Imports
+import Appbar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+
+export class NavBar extends Component {
+  render() {
+    return (
+      <Appbar>
+        <Toolbar className="nav-container">
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/signup">
+            Signup
+          </Button>
+        </Toolbar>
+      </Appbar>
+    );
+  }
+}
+
+export default NavBar;
