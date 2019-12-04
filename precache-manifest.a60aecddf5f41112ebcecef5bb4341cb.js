06bfb85d3c2bdb1ef43c231e7037a46c23008346
Isaiah Scheel <isaiahscheel@gmail.com>
2019-12-03 23:08:43 -0800

    Updates

---
+++ precache-manifest.a60aecddf5f41112ebcecef5bb4341cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4536a41a47fefa9dd72a6fa0f39c46",
+    "url": "/SummitLog-React/index.html"
+  },
+  {
+    "revision": "8ee9458cf238a2d3a804",
+    "url": "/SummitLog-React/static/css/main.75be03e8.chunk.css"
+  },
+  {
+    "revision": "b12469066a4b9ab59812",
+    "url": "/SummitLog-React/static/js/2.8b58de8c.chunk.js"
+  },
+  {
+    "revision": "8ee9458cf238a2d3a804",
+    "url": "/SummitLog-React/static/js/main.a9c2ef3b.chunk.js"
+  },
+  {
+    "revision": "427763e2836e4962f6a6",
+    "url": "/SummitLog-React/static/js/runtime~main.e6dcf36f.js"
+  },
+  {
+    "revision": "391ed446526d2f2ac50a9e21e6014979",
+    "url": "/SummitLog-React/static/media/logo.391ed446.png"
+  }
+]);
